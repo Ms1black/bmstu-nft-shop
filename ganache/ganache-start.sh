0936223c693dd235cfbb865c5a247c0fdf5065aa
Ms black <li304ko3@gmail.com>
2025-03-23 20:47:22 +0300

    fix: add CORS

--- ganache/ganache-start.sh
+++ ganache/ganache-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Запуск Ganache"
-ganache-cli -a 1 -e 1000 --gaslimit 6721975 --host 0.0.0.0 --port 8545
+ganache-cli -a 1 -e 1000 --gaslimit 6721975 --host 0.0.0.0 --port 8545 --cors "*"
