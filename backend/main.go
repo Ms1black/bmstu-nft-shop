008d96282160ced1b6cb85c3ff3412af10d2068c
Liza <li304ko3@gmail.com>
2025-03-23 21:38:12 +0300

    fix: buid

--- backend/main.go
+++ backend/main.go
@@ -12,9 +12,5 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-	log.Println("Starting HTTPS server on https://localhost:8080")
-	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
-	if err != nil {
-		log.Fatal("Error starting HTTPS server:", err)
-	}
+	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
 }
