674bd8313c343d61f96137bc67e3e6d564cfe8df
Liza <li304ko3@gmail.com>
2025-03-23 17:36:59 +0300

    feat: second part ci/cd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [balance, setBalance] = useState("0");
+
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const web3 = new Web3("http://127.0.0.1:8545");
+      const accounts = await web3.eth.getAccounts();
+      if (accounts.length > 0) {
+        const balanceWei = await web3.eth.getBalance(accounts[0]);
+        const balanceEth = web3.utils.fromWei(balanceWei, "ether");
+        setBalance(balanceEth);
+      }
+    };
+
+    loadBlockchainData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Баланс пользователя:</h2>
+      <p>{balance} ETH</p>
     </div>
   );
-}
+};
 
 export default App;
