5a5da73a9268a4e1e2f4fcb91a4d867362910499
Liza <li304ko3@gmail.com>
2025-03-24 09:44:05 +0300

    fix: fix frontend path

--- backend/main.go
+++ backend/main.go
@@ -1,30 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/rs/cors"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"world\"}"))
-	})
-
-	// cors.Default() setup the middleware with default options being
-	// all origins accepted with simple methods (GET, POST). See
-	// documentation below for more options.
-	handler := cors.Default().Handler(mux)
-	http.ListenAndServeTLS(":443", "cert.pem", "key.pem", handler)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "BMSTU NFT Store Backend")
 }
 
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "BMSTU NFT Store Backend")
-// }
-
-// func main() {
-// 	http.HandleFunc("/", homeHandler)
-// 	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
-// }
+func main() {
+	http.HandleFunc("/", homeHandler)
+	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
+}
