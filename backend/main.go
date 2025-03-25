d6736f3d1b4ca503c60d6f5bb45ebf547dbedebe
Liza <li304ko3@gmail.com>
2025-03-25 20:47:10 +0300

    fix: need to processing backend

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,16 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "BMSTU NFT Store Backend")
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "BMSTU NFT Store Backend")
+// }
 
-func main() {
-	http.HandleFunc("/", homeHandler)
-	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
-}
+// func main() {
+// 	http.HandleFunc("/", homeHandler)
+// 	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
+// }
